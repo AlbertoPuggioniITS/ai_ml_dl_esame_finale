010de62bef58880024753cacf28e2cea671f1821
Alberto <puggioni.alberto@gmail.com>
2023-11-08 10:48:10 +0100

    Correzioni

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Per far funzionare il progetto, assicurarsi di installare tutte le librerie
 # Tutti i path sono riferiti alla macchina con la quale è stata sviluppata l'applicazione
 
-
-
-
-
-
+# SWAGGER #
+# Spostarsi nella directory DB o modello e poi lanciare il seguente comando
+# uvicorn api:app --host 0.0.0.0 --port 8000
+# dirigersi poi sul seguente URL --> http://0.0.0.0:8000/docs#/
